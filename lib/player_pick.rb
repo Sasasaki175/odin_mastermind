e16111be8c00e4564712e4f29eb1656f0d556881
Sasasaki175 <207643966+Sasasaki175@users.noreply.github.com>
2025-07-22 15:08:34 +0900

    Change player_input to instance variable to make it work

--- lib/player_pick.rb
+++ lib/player_pick.rb
@@ -1,16 +1,18 @@
 class PlayerPick
+  PICK_COLORS = %w[b g o p r y]
+
   def get_player_input
-    puts 'Choose 4 pick colors from Blue, Green, Orange, Purple, Red, Yellow and concatenate the initials. e.g. bggr, rbrp, yogp.'
     loop do
-      player_input = gets.chomp.downcase
-      break if valid_player_pick?(player_input)
+      @player_input = gets.chomp.downcase
+      break if valid_player_pick?(@player_input)
       puts 'Invalid input please try again.'
     end
-    player_input
+    
+    @player_input
   end
 
   private
-  
+
   def valid_player_pick?(pick)
     pick.length == 4 && pick.chars.all? { |chr| PICK_COLORS.include?(chr) }
   end
