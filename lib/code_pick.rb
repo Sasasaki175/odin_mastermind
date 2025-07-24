cfdfee1d349fc05a78efbba34c181197444a50d7
Sasasaki175 <207643966+Sasasaki175@users.noreply.github.com>
2025-07-24 15:48:01 +0900

    Add com_pick method so the computer can play the Codebreaker

--- lib/code_pick.rb
+++ lib/code_pick.rb
@@ -1,6 +1,7 @@
 class CodePick
   COLORS = %w[b g o p r y].freeze
 
+  #Input code manually
   def get_player_input
     loop do
       @player_input = gets.chomp.downcase
@@ -14,6 +15,12 @@ class CodePick
   #Picks 4 random picks
   def random_pick
     Array.new(4) { COLORS.sample }.join
+  end
+  
+  #Returns 
+  def com_pick(match_arr)
+    com_pick = match_arr.map { |pick| pick != nil ? pick : COLORS.sample }
+    com_pick.join
   end
 
   private
