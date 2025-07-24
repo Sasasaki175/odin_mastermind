9bf675a17ad952fd06834f96aa0573e4348b6b43
Sasasaki175 <207643966+Sasasaki175@users.noreply.github.com>
2025-07-24 09:27:31 +0900

    Modify random_pick method (previously cpu_pick) Changed method name of cpu_pick to random_pick to be more accurate. And added .join to the return value of random_pick so it returns a string instead of an array.

--- lib/secret_code.rb
+++ lib/secret_code.rb
@@ -1,8 +1,8 @@
 class SecretCode
-  PICK_COLORS = %w[b g o p r y]
+  PICK_COLORS = %w[b g o p r y].freeze
 
   #Computer picks 4 random picks
-  def cpu_pick
-    Array.new(4) { PICK_COLORS.sample }
+  def random_pick
+    Array.new(4) { PICK_COLORS.sample }.join
   end
 end
