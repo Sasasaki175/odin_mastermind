b2dc85cfa1e50600864b1decc2f70a950b44b373
Sasasaki175 <207643966+Sasasaki175@users.noreply.github.com>
2025-07-24 16:28:40 +0900

    Fix some style to follow rubocop

--- lib/code_pick.rb
+++ lib/code_pick.rb
@@ -1,25 +1,26 @@
 class CodePick
   COLORS = %w[b g o p r y].freeze
 
-  #Input code manually
-  def get_player_input
+  # Input code manually
+  def player_input
     loop do
       @player_input = gets.chomp.downcase
       break if valid_player_pick?(@player_input)
+      
       puts 'Invalid input please try again.'
     end
 
     @player_input
   end
 
-  #Picks 4 random picks
+  # Picks 4 random picks
   def random_pick
     Array.new(4) { COLORS.sample }.join
   end
   
-  #Returns 
+  # Returns random string with correctly matched colors fixed
   def com_pick(match_arr)
-    com_pick = match_arr.map { |pick| pick != nil ? pick : COLORS.sample }
+    com_pick = match_arr.map { |pick| !pick.nil? ? pick : COLORS.sample }
     com_pick.join
   end
 
