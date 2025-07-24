2ab7008b9e95c076bcb0e887f0f3b9f98e053a3f
Sasasaki175 <207643966+Sasasaki175@users.noreply.github.com>
2025-07-24 15:45:35 +0900

    Make the computer play the Codebreaker Now when you choose to play as the Codemaker the computer would play the Codebreaker.

--- mastermind.rb
+++ mastermind.rb
@@ -2,12 +2,21 @@ require_relative 'lib/code_pick'
 
 class MasterMind
   def game_start
+    choose_role
+    play_game
+  end
+
+  private
+
+  def choose_role
     puts 'Do you want to play as Codebreaker or Codemaker.'
     puts "Input Codebreaker: 'b' or Codemaker: 'm'"
 
     loop do
       @player_role = gets.chomp
+
       break if ['b', 'm'].include?(@player_role)
+
       puts 'Invalid input please try again.'
     end
 
@@ -17,14 +26,12 @@ class MasterMind
       puts 'Choose 4 pick colors from Blue, Green, Orange, Purple, Red, Yellow and concatenate the initials as secret code. e.g. bggr, rbrp, yogp.'
       @secret_code = CodePick.new.get_player_input
     end
-
-    play_game
   end
-
-  private
 
   def play_game
     guesses_left = 10
+
+    @matched_picks = Array.new(4)
 
     if @player_role == 'b'
       puts 'Choose 4 pick colors from Blue, Green, Orange, Purple, Red, Yellow and concatenate the initials. e.g. bggr, rbrp, yogp.'
@@ -32,14 +39,29 @@ class MasterMind
     
     #You can guess the code 10 times until you get it correct or game over
     10.times do
-      @breaker_pick = CodePick.new.get_player_input
+      get_breaker_pick
+
       guesses_left -= 1
+
       puts "Correct position: #{match_chr(@secret_code, @breaker_pick)} Include number: #{include_chr(@secret_code, @breaker_pick)}"
+      
+      @matched_picks = matching_picks(@secret_code, @breaker_pick)
+
       break if @secret_code == @breaker_pick
-      puts "Guesses left: #{guesses_left} Try again."
+
+      puts "Guesses left: #{guesses_left}"
+      puts 'Try again.' if guesses_left > 0
     end
 
     did_breaker_win
+  end
+
+  def get_breaker_pick
+    if @player_role == 'b'
+      @breaker_pick = CodePick.new.get_player_input
+    else
+      @breaker_pick = CodePick.new.com_pick(@matched_picks)
+    end
   end
 
   #Compare two strings and return number of characters that match including position
@@ -50,17 +72,32 @@ class MasterMind
   #Count and return number of shared characters 
   def include_chr(string_a, string_b)
     inclusion = 0
+
     for chr in string_b.chars.uniq
       inclusion += [string_a.count(chr), string_b.count(chr)].min
     end
+
     inclusion
+  end
+
+  #Returns an array with the matched color picks and nil
+  def matching_picks(string_a, string_b)
+    4.times.map { |i| string_a[i] == string_b[i] ? string_a[i] : nil }
   end
 
   def did_breaker_win
     if @secret_code == @breaker_pick
-      puts "You win!"
+      if @player_role == 'b'
+        puts 'You win!'
+      else
+        puts 'Com wins!'
+      end
     else
-      puts "You lose"
+      if @player_role == 'b'
+        puts 'You lose'
+      else
+        puts 'Com could not figure out the code'
+      end
     end
   end
 end
